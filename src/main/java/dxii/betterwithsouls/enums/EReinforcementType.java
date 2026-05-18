3df416c614775d7b3a80a7c1381dac7c6b23fb76
dx512ii <94173166+dx512ii@users.noreply.github.com>
2026-05-18 16:29:43 +0300

    update to examplemod 8.0

--- src/main/java/dxii/betterwithsouls/enums/EReinforcementType.java
+++
@@ -1,7 +0,0 @@
-package dxii.betterwithsouls.enums;
-
-public enum EReinforcementType {
-	NORMAL,
-	UNIQUE,
-	NONE
-}
--- src/main/java/dxii/betterwithsouls/interfaces/IWorld.java
+++
@@ -1,7 +0,0 @@
-package dxii.betterwithsouls.interfaces;
-
-import dxii.betterwithsouls.util.DynamicLightsModule;
-
-public interface IWorld {
-	DynamicLightsModule bws$getDynLights();
-}
